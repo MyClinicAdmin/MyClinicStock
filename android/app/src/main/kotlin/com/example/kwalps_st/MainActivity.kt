1010d2bfcedea46a06b8831cbfef76b1a9ce154e
Fonseca Chimuco Moma Nguelengue <aurelionguelenguea@gmail.com>
2025-10-06 18:53:28 -0700

    Deploy Web a partir do backup (release atual)

--- android/app/src/main/kotlin/com/example/kwalps_st/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.kwalps_st
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/branding.dart
+++
@@ -1,4 +0,0 @@
-class Brand {
-  static const appName = 'MY STOCK';
-  static const logo = 'assets/branding/my_stock_logo.png';
-}
